b67225a9d18f6c622feb8bd73225046fa5499345
johnvanschultz <jvansch1@binghamton.edu>
2018-09-02 17:04:18 -0400

    Move loop logic to Bank class.

--- src/com/company/Bank.java
+++ src/com/company/Bank.java
@@ -4,8 +4,12 @@ import java.io.FileNotFoundException;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Bank {
+
+    Account account;
+
     public Account getAccount(String name) {
         Account account = null;
         try {
@@ -21,5 +25,51 @@ public class Bank {
             System.out.println("New Account Created!");
         }
         return account;
+    }
+
+    public void bankLoop(Account account) {
+        int userInput = 0;
+        while (userInput != 4) {
+            printMenu();
+            userInput = getUserInput();
+            handleUserDecision(account, userInput);
+        }
+    }
+
+    public String getAccountName() {
+        System.out.println("Please enter account name");
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        return s;
+    }
+
+    public void handleUserDecision(Account account, int choice) {
+        switch(choice) {
+            case 1:
+                account.handleDeposit();
+                break;
+            case 2:
+                account.handleWithdraw();
+                break;
+            case 3:
+                System.out.println(account.getBalance());
+                break;
+        }
+    }
+
+    public int getUserInput() {
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        return Integer.parseInt(s);
+    }
+
+    public void printMenu() {
+        System.out.println("Please select an option: ");
+        System.out.println();
+        System.out.println("1. Deposit");
+        System.out.println("2. Withdraw");
+        System.out.println("3. Balance");
+        System.out.println("4. Exit");
+        System.out.println("> ");
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,57 +6,11 @@ public class Main {
 
     public static void main(String[] args) {
         Bank bank = new Bank();
-        String name = getAccountName();
+        String name = bank.getAccountName();
 	    Account account = bank.getAccount(name);
 	    if (account != null) {
-	        bankLoop(account);
+	        bank.bankLoop(account);
         }
 
-    }
-
-    public static void bankLoop(Account account) {
-        int userInput = 0;
-        while (userInput != 4) {
-            printMenu();
-            userInput = getUserInput();
-            handleUserDecision(account, userInput);
-        }
-    }
-
-    public static String getAccountName() {
-        System.out.println("Please enter account name");
-        Scanner in = new Scanner(System.in);
-        String s = in.nextLine();
-        return s;
-    }
-
-    public static void handleUserDecision(Account account, int choice) {
-        switch(choice) {
-            case 1:
-                account.handleDeposit();
-                break;
-            case 2:
-                account.handleWithdraw();
-                break;
-            case 3:
-                System.out.println(account.getBalance());
-                break;
-        }
-    }
-
-    public static int getUserInput() {
-        Scanner in = new Scanner(System.in);
-        String s = in.nextLine();
-        return Integer.parseInt(s);
-    }
-
-    public static void printMenu() {
-        System.out.println("Please select an option: ");
-        System.out.println();
-        System.out.println("1. Deposit");
-        System.out.println("2. Withdraw");
-        System.out.println("3. Balance");
-        System.out.println("4. Exit");
-        System.out.println("> ");
     }
 }
